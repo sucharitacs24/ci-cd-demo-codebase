c2fd76451a9fc72178ec7f4df08691ea81867f93
Sucharita <sucharita.cs24@bmsce.ac.in>
2026-09-18 14:04:26 +0530

    Add tests for power function in math.test.js
    Added tests for the power function including positive exponent, exponent of 1, and exponent of 0.

--- math.test.js
+++ math.test.js
@@ -1,3 +1,4 @@
+const { add, subtract, multiply, divide, power } = require('./math');
 const test = require('node:test');
 const assert = require('node:assert');
 const { add, subtract, multiply, divide } = require('./math');
@@ -20,4 +21,15 @@ test('divide works', () => {
 
 test('divide by zero gives an error', () => {
   assert.throws(() => divide(5, 0));
+});
+test('power calculates positive exponent', () => {
+  assert.equal(power(2, 3), 8);
+});
+
+test('power calculates exponent of 1', () => {
+  assert.equal(power(5, 1), 5);
+});
+
+test('power with exponent 0 returns 1', () => {
+  assert.equal(power(7, 0), 1);
 });
